ddbcf985b4985239055999134b26de4faf30d57f
futashikana <joseph.stratford@ucas-edu.net>
2026-01-10 04:28:22 +0000

    5 finance finished im never getting my dad to test my code ever again

--- individual_projects/finance_calculator.py
+++ individual_projects/finance_calculator.py
@@ -1,14 +1,44 @@
 # JS, 1st, Financial Calculator
+
 # ----- PSUEDOCODE -----
 # Create savings function
+#   ask user for saving goal
+#   ask for how often the user is contributing
+#   ask the user how much they are contributing each time
+#   calculate result and print it
 # Create cic function
+#   ask user their starting amount
+#   ask user for their interest rate percent
+#   ask user for time compounding
+#   define cycle function
+#       find the calculation for each year
+#   call cycle function for every year
+#   calculate result and print it
+# Create budget function
+#   ask user for number of budget categories
+#   ask user for each category name
+#   ask user for monthly income
+#   ask user for percentage of each category
+#   calculate result and print it
+# Create sale_price function
+#   ask user for cost of item
+#   ask user for percent discount
+#   calculate result and print it
+# Create tip function
+#   ask user for bill price
+#   ask user for percent tip
+#   calculate result and print it
+# Create an infinite loop
+#   ask user for calculator choice
+#   call selected calculator function
+
 # ----- CODE -----
 def savings():
     print("Welcome to the Savings Goal Calculator")
     goal = input("What amount are you saving to: ").strip().strip("$")
     while True:
         try:
-            goal = int(goal)
+            goal = float(goal)
             break
         except:
             goal = input("That was not a valid option. Please try again\n").strip().strip("$")
@@ -18,7 +48,7 @@ def savings():
     deposit = input("How much are you contributing each time: ").strip().strip("$")
     while True:
         try:
-            deposit = int(deposit)
+            deposit = float(deposit)
             break
         except:
             deposit = input("That was not a valid option. Please try again\n").strip().strip("$")
@@ -28,20 +58,20 @@ def savings():
     while count < goal:
         count += deposit
         result += 1 
-    print(f"It will take {result:.2f} {"weeks" if time == "1" else "months" if time == "2" else "half-years" if time == "3" else "years"} to save ${goal}.")
+    print(f"It will take ~{result:.2f} {"weeks" if time == "1" else "months" if time == "2" else "half-years" if time == "3" else "years"} to save ${goal:.2f}.")
 def cic():
     print("Welcome to the Compound Interest Calculator")
     starting = input("Starting Amount: ").strip().strip("$")
     while True:
         try:
-            starting = int(starting)
+            starting = float(starting)
             break
         except:
             starting = input("That was not a valid option. Please try again\n").strip().strip("$")
     interest = input("Interest Rate Percent: ").strip().strip("%")
     while True:
         try:
-            interest = int(interest)
+            interest = float(interest)
             break
         except:
             interest = input("That was not a valid option. Please try again\n").strip().strip("$")
@@ -52,13 +82,15 @@ def cic():
             break
         except:
             years = input("That was not a valid option. Please try again\n").strip()
+    result = starting
     def cycle():
-        pass
-        #CALCULATION
+        nonlocal result
+        result *= (1 + interest / 100) 
+        return result
     for _ in range(years):
-        cycle()
+        result = cycle()
     print("\033c", end="")
-    print(f"At the end of {years} years you will have $result:.2f.")
+    print(f"At the end of {years} years you will have ${result:.2f}")
 def budget():
     print("Welcome to the Budget Allocator")
     num = input("How many budget categories do you have: ").strip()
@@ -68,26 +100,40 @@ def budget():
             break
         except:
             num = input("That was not a valid option. Please try again\n").strip()
+    categories = []
     for i in range(num):
-        categories = []
-        categories.append(input(f"Category {i}: ").strip())
+        categories.append(input(f"Category {i+1}: ").strip())
     income = input("What is your monthly income: ").strip().strip("$")
-    while not isinstance(income, int):
-        income = input("That was not a valid option. Please try again\n")
+    while True:
+        try:
+            income = float(income)
+            break
+        except:
+            income = input("That was not a valid option. Please try again\n").strip().strip("$")
+    percentages = []
     for category in categories:
-        percentages = []
         percent = input(f"What percent is {category}: ").strip().strip("%")
         while True:
             try:
-                percent = int(percent)
+                percent = float(percent)
                 break
             except:
                 percent = input("That was not a valid option. Please try again\n").strip().strip("%")
         percentages.append(percent)
+    collective = 0
+    for i in percentages:
+        collective += i
     print("\033c", end="")
+    print(f"Your collective percentage is {collective:.2f}")
+    if collective > 100:
+        again = input("Your collective percentage is over 100. Are you sure you want to continue? (yes/retry) ")
+        if again == "retry":
+            print("\033c", end="")
+            budget()
     for i in range(num):
-        result = income*percentages[i]
-        print(f"{category} is {result}", end=", ")
+        result = income * (percentages[i] / 100)
+        print(f"{categories[i]} is {result:.2f}", end=", ")
+        print()
 def sale_price():
     print("Welcome to the Sale Price Calculator")
     cost = input("How much does the item originally cost: ").strip().strip("$")
@@ -106,31 +152,32 @@ def sale_price():
             discount = input("That was not a valid option. Please try again\n").strip().strip("%")
     print("\033c", end="")
     result = cost - (cost * (discount / 100))
-    print(f"The item now costs ${result}")
+    print(f"The item now costs ${result:.2f}")
 def tip():
     print("Welcome to the Tip Calculator")
     bill = input("How much is the bill: ").strip().strip("$")
     while True:
         try:
-            bill = int(bill)
+            bill = float(bill)
             break
         except:
             bill = input("That was not a valid option. Please try again\n").strip().strip("$")
     tip = input("What percent of a tip are you giving: ").strip().strip("%")
     while True:
         try:
-            tip = int(tip)
+            tip = float(tip)
             break
         except:
             tip = input("That was not a valid option. Please try again\n").strip().strip("%")
     print("\033c", end="")
     result = bill + (bill * (tip / 100))
-    print(f"The item now costs ${result}")
+    print(f"With the original bill being ${bill:.2f}, and the {tip}% tip being ${(bill * (tip / 100)):.2f}, your total is now ${result:.2f}")
 
 print("\033c", end="")
 while True:
-    calc = input("Enter the number so select an option\n 1. Savings Time Calculator\n 2. Compound Interest Calculator\n 3. Budget Allocator\n 4. Sale Price Calculator\n 5. Tip Calculator\n 6. Exit\n")
+    calc = input("Enter the number to select an option\n 1. Savings Time Calculator\n 2. Compound Interest Calculator\n 3. Budget Allocator\n 4. Sale Price Calculator\n 5. Tip Calculator\n 6. Exit\n")
     if calc == "6":
+        print("\033c", end="")
         print("Thank you for using this program.")
         break
     while calc != "1" and calc != "2" and calc != "3" and calc != "4" and calc != "5":
